04bcfde719fb4e078cbf7c3318c7997996141c9c
wytian <tian.wesley@gmail.com>
2018-03-07 21:58:24 -0500

    Exponentiation test
    Added basic exponentiation test

--- test_rpn.py
+++ test_rpn.py
@@ -6,3 +6,7 @@ class TestBasics(unittest.TestCase):
         result = rpn.calculate("1 1 +")
         self.assertEqual(2, result)
 
+    def test_exponent(self):
+	    result = rpn.calculate("2 3 *")
+	    self.assertEqual(8, result)
+
